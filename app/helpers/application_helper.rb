eba8087ff21c11849eb13dbe34d9246492757ebf
ura <byorickq@gmail.com>
2013-11-27 17:57:15 +0200

    title tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :hard_wrap => true, :filter_html => true, :no_intraemphasis => true, :gh_blockcode => true)
 		raw markdown.render(text)
 	end
+
+	def title(page_title)
+		content_for :title, page_title.to_s + " • "
+	end
 end
